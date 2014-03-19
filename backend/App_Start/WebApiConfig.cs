46c39c76baa8e9dea4f5a79e3d5a07fb8945ec7c
Richard <yuchi_shen@hotmail.com>
2014-03-19 19:02:58 +0000

    add client site, add CORS to api ,  sample api call on client site

--- backend/App_Start/WebApiConfig.cs
+++ backend/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace backend
 {
@@ -18,6 +19,9 @@ namespace backend
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Web API routes
+            var cors = new EnableCorsAttribute("*", "*", "GET");
+            config.EnableCors(cors);
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
